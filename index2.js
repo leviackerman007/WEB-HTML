59603fc36f264acbc36f0ac5c0ce22a1b1b884c8
leviackerman007 <kgs.tushar522@gmail.com>
2025-12-20 06:58:12 +0000

    Correction

--- index2.js
+++ index2.js
@@ -2,8 +2,8 @@ import express from "express"
 import productRoutes from "./routes/product.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import postRoutes from "./routes/post.routes.js"
-// import { connectDB } from "./db/db.js";
-// connectDB()
+import { connectDB } from "./db/db.js";
+connectDB()
 const app = express();
 
 app.use(express.json())
