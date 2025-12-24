e9792cf5f06920418a1c07fdbb3645631fcfefc6
leviackerman007 <kgs.tushar522@gmail.com>
2025-12-24 19:07:57 +0000

    Post controller 1. createPost 2. allPost

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,8 +1,10 @@
-import { allPost } from "../controllers/post.controller.js";
+import { allPost,createPost } from "../controllers/post.controller.js";
 import Router from "express"
 
 const router=Router()
 
-router.get("/allpost",allPost)
+router.get("/allPost",allPost)
+
+router.post("/createPost",createPost)
 
 export default router;
